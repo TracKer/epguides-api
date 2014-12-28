aae46f3489d09e46b7128a711a8590eeda7d8ddf
Andrey Rudenko <tracker2k@gmail.com>
2014-12-29 01:21:55 +0200

    #2: Add "Remove other" functionality from episodes list

--- lib/EpGuidesAPI/Show.php
+++ lib/EpGuidesAPI/Show.php
@@ -7,10 +7,11 @@ class Show {
   private $epguides_id;
   private $title;
   private $imdb_id;
-  private $raw_data = array('page' => null, 'csv' => null);
+  private $raw_data = array('page' => null, 'csv' => null, 'printable' => null);
   private $episodes = array();
   private $remove_specials;
   private $remove_other;
+  private $printable_episodes;
 
   /**
    * Constructor
@@ -19,10 +20,13 @@ class Show {
    *   Machine name of tv show can be taken from url on epguides.com.
    * @param bool $remove_specials
    *   Flag which removes Specials from episodes list.
+   * @param bool $remove_other
+   *   Flag which removes other stuff from episodes list.
    */
-  public function __construct($epguides_name, $remove_specials = true) {
+  public function __construct($epguides_name, $remove_specials = true, $remove_other = true) {
     $this->epguides_name = $epguides_name;
     $this->remove_specials = $remove_specials;
+    $this->remove_other = $remove_other;
     $this->parseData();
   }
 
@@ -78,6 +82,30 @@ class Show {
     $tmp_csv = $this->parseCSV($tmp_csv);
     $this->raw_data['csv'] = $tmp_csv;
     unset($tmp_csv);
+
+    // If "Remove other" flag is set, then we have to download printable
+    // version from tvrage.com.
+    if ($this->remove_other) {
+      $this->raw_data['printable'] = $this->downloadURL("http://www.tvrage.com/shows/id-$this->epguides_id/printable");
+
+      $subject = $this->raw_data['printable'];
+      $pattern = '/>[\d]+ :([\d]{2})x([\d]{2}) - (.*)<\//U';
+      preg_match_all($pattern, $subject, $matches);
+      unset($subject);
+      unset($pattern);
+      if (empty($matches[1])) {
+        throw new Exception('Unable to parse printable version from tvrage.com.');
+      }
+      $this->printable_episodes = array();
+      foreach ($matches[1] as $key => $tmp_item) {
+        $season = intval($matches[1][$key]);
+        $episode = intval($matches[2][$key]);
+        $title = trim($matches[3][$key]);
+        $this->printable_episodes[$season][$episode] = $title;
+      }
+      dump($this->printable_episodes);
+      unset($matches);
+    }
   }
 
   private function parseCSV($CSVData) {
@@ -107,7 +135,8 @@ class Show {
       CURLOPT_URL => $url,
       CURLOPT_HEADER => 0,
       CURLOPT_RETURNTRANSFER => TRUE,
-      CURLOPT_TIMEOUT => 4
+      CURLOPT_TIMEOUT => 4,
+      CURLOPT_FOLLOWLOCATION => true,
     );
 
     $ch = curl_init();
@@ -154,10 +183,19 @@ class Show {
     $episodes = array();
     foreach ($this->raw_data['csv'] as $episode_data) {
       $episode = new Episode($this, $episode_data);
+
+      // Remove Specials.
       if ($this->remove_specials && $episode->isSpecial()) {
         unset($episode);
         continue;
       }
+
+      // Remove other.
+      if ($this->remove_specials && (!isset($this->printable_episodes[$episode->getSeason()][$episode->getEpisode()]))) {
+        unset($episode);
+        continue;
+      }
+
       $episodes[] = $episode;
     }
 
